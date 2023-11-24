b9bb308b03452f1fe79c68b4c94d1b02e5393b36
Sarthak Manhas <146426131+Sarthak4126@users.noreply.github.com>
2023-11-24 13:48:31 +0530

    Add files via upload

---
+++ file.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter the size of the array");
+    scanf("%d",&n);
+    int a[n],i;
+    printf("enter the array of elements");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    int j;
+    for(i=0;i<n-1;i++)
+    {
+        int k=0;
+        for(j=k+1;j<n-1;j++)
+        {
+            if(a[k]>a[j])
+            {
+                int t=a[j];
+                a[j]=a[k];
+                a[k]=t;
+            }
+            k++;
+        }
+    }   
+    printf("the sorted array");
+    for(i=0;i<n;i++)
+    printf("%d\n",a[i]);
+ 
+}
