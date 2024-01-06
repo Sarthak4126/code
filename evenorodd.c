7c4d25c28ba8b2ece20330e69b9cf694bcb4fc1b
Sarthak Manhas <146426131+Sarthak4126@users.noreply.github.com>
2024-01-06 13:53:48 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("the no is even or odd");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("the no is even");
+    else
+    printf("the no is odd");
+}
